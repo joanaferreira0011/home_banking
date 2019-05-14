38d2353fc04ee38e069ef90d1884cace16188822
Claudia <claudiainescostamartins@gmail.com>
2019-05-14 23:53:59 +0100

    generate_hash function

--- src/account.c
+++ src/account.c
@@ -1,38 +1,27 @@
+#include "account.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "account.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 
-FILE *executeCommand(char *file, char *command)
+#define READ 0
+#define WRITE 1
+
+void err_sys(char *msg)
 {
-  FILE *aux = NULL;
-  char cmd[256];
-  strcpy(cmd, command);
-  strcat(cmd, file);
-  aux = popen(cmd, "r");
-
-  return aux;
-}
-
-char *getSha256Sum(char *str)
-{
-  char command[] = "sha256sum ";
-  FILE *sha256sum = executeCommand(str, command);
-  char aux[256] = "";
-  fgets(aux, 255, sha256sum);
-
-  char *sha256 = strndup(aux, strlen(aux) - strlen(str) - 3);
-
-  char *pos;
-  if ((pos = strchr(sha256, '\n')) != NULL)
-    *pos = '\0';
-
-  fclose(sha256sum);
-  return sha256;
+  fprintf(stderr, "%s\n", msg);
+  exit(1);
 }
 
 char *generate_salt()
 {
-  char *salt = malloc (sizeof (char) * SALT_LEN);
+  char *salt = malloc(sizeof(char) * SALT_LEN);
   const char *hex_digits = "0123456789ABCDEF";
   char character[2];
   int i;
@@ -40,31 +29,61 @@ char *generate_salt()
   for (i = 0; i < SALT_LEN; i++)
   {
     character[0] = hex_digits[(rand() % 16)];
-    //printf("%s\n", character);
     fflush(stdout);
     strcat(salt, character);
-    //printf("%s\n", salt);
     fflush(stdout);
   }
 
   return salt;
 }
 
+char *generate_hash(char *str)
+{
+  char *hash = malloc(sizeof(char) * (SALT_LEN + MAX_PASSWORD_LEN));
+
+  int fd1[2], fd2[2];
+  pid_t pid;
+
+  if (pipe(fd1) < 0 || pipe(fd2) < 0)
+    err_sys("pipe error");
+  if ((pid = fork()) < 0)
+    err_sys("fork error");
+
+  if (pid == 0) /*child*/
+  {
+    close(fd2[WRITE]);
+    close(fd1[READ]);
+    dup2(fd2[READ], STDIN_FILENO);
+    dup2(fd1[WRITE], STDOUT_FILENO);
+    execlp("sha256sum", "sha256sum", NULL);
+    exit(0);
+  }
+  else /*parent*/
+  {
+    close(fd1[WRITE]);
+    close(fd2[READ]);
+    write(fd2[WRITE], str, HASH_LEN);
+    close(fd2[WRITE]);
+    read(fd1[READ], hash, HASH_LEN);
+    hash[HASH_LEN] = '\0';
+    printf("%s\n", hash);
+  }
+
+  return hash;
+}
+
 struct bank_account create_account(int id, float balance, char password[MAX_PASSWORD_LEN])
 {
-  printf("enter");
   fflush(stdout);
   bank_account_t a;
   a.account_id = id;
   a.balance = balance;
-  printf("balance");
   fflush(stdout);
   strcpy(a.salt, generate_salt());
-  printf("salt");
   fflush(stdout);
-  char * aux = a.salt;
+  char *aux = a.salt;
   strcat(aux, password);
-  strcpy(a.hash, getSha256Sum(aux));
+  strcpy(a.hash, generate_hash(aux));
 
   return a;
 }
