e087ec79f1c5df44aa3ad3b78f7c59aff6037ca5
João Matos <joao.nuno.matos@fe.up.pt>
2019-05-15 20:12:51 +0100

    more progress (i hope)

--- user_src/args.c
+++ user_src/args.c
@@ -1,55 +1,111 @@
 #include "args.h"
 #include "sope.h"
+#include "unistd.h"
+#include "string.h"
+#include "stdlib.h"
+#include "stdint.h"
 
-#define USERID_INDEX 1
+#define ACCID_INDEX 1
 #define PASSWORD_INDEX 2
 #define OPDELAY_INDEX 3
 #define OPCODE_INDEX 4
 #define OPARGS_INDEX 5
-
-/* Extracts positive number / zero from string and returns it, will return -1 if an error occurs */
-int non_neg_atoi(char *str) {
-    return -1;
-}
+#define __ATOUL(STR) strtoul(STR, NULL, 10)
 
 /* Separates the arguments from an argument string */
-char **separate_args(char *arg_str) {
-    return 0;
-}
+char **separate_args(char *arg_str);
 
-int process_arguments(int argc, char **argv, tlv_request_t *buffer) {
-    if (argc != EXPECTED_ARGC || argv == NULL || buffer == NULL) {
-        return 1;
-    }
+void string_array_free_elements(char **array);
+
+req_header_t new_header(char *account_id, char *password, char *op_delay);
+
+req_create_account_t new_create_account_message(char **operation_arguments);
+
+req_transfer_t new_transfer_message(char **operation_arguments);
+
+
+tlv_request_t process_arguments(char **argv) {
+    req_value_t value;
+    value.header = new_header (
+        argv[ACCID_INDEX],
+        argv[PASSWORD_INDEX],
+        argv[OPDELAY_INDEX]
+    );
     
-    int userid;
-    if ((userid = non_neg_atoi(argv[USERID_INDEX])) == -1) {
-    	return 1;
-    }
-    
-    int opdelay;
-    if ((opdelay = non_neg_atoi(argv[OPDELAY_INDEX]) == -1)) {
-    	return 1;
-    }
-    
-    op_type_t opcode = non_neg_atoi(argv[OPCODE_INDEX]);
+    op_type_t operation = __ATOUL(argv[OPCODE_INDEX]);
     char **opargs = separate_args(argv[OPARGS_INDEX]);
-    switch (opcode) {
-    	case OP_CREATE_ACCOUNT:
-            
-    		break;
-    	case OP_BALANCE:
-    		break;
-    	case OP_TRANSFER:
-    		break;
-    	case OP_SHUTDOWN:
-    		break;
-    	default:
-    		break;
+    switch (operation) {
+        case OP_CREATE_ACCOUNT:
+            value.create = new_create_account_message(opargs);
+            break;
+        case OP_TRANSFER:
+            value.transfer = new_transfer_message(opargs);
+            break;
     }
+    string_array_free_elements(opargs);
 
-    return 0;
+    tlv_request_t request;
+    request.type = operation;
+    request.length = sizeof(req_value_t);
+    request.value = value;
+    return request;
 }
 
+char **separate_args(char *arg_str) {
+    if (!(strlen(arg_str) > 0)) {
+        return NULL;
+    }
+    
+    char **array = malloc(4*sizeof(char*));
+    if (array == NULL) {
+        return NULL;
+    }
+
+    array[0] = strtok(arg_str, " ");
+    int index = 1;
+    while (((array[index] = strtok(NULL, " ")) != NULL) && (index < 4)) {
+        ++index;
+    }
+
+    array[3] = NULL;
+
+    return array;
+}
+
+void string_array_free_elements(char **array) {
+    if (array == NULL) {
+        return;
+    }
+    for (int i = 0; array(i) != NULL; ++i) {
+        free(array[i]);
+    }
+    free(array);
+}
+
+req_header_t new_header(char *account_id, char *password, char *op_delay) {
+    req_header_t header;
+    header.pid = getpid();
+    header.account_id = __ATOUL(account_id);;
+    strncpy(header.password,password, MAX_PASSWORD_LEN);
+    header.password[PASSWORD_LENGTH] = '\0';
+    header.op_delay_ms = __ATOUL(account_id);;
+    return header;
+}
+
+req_create_account_t new_create_account_message(char **operation_arguments) {
+    req_create_account_t create;
+    create.account_id = __ATOUL(operation_arguments[0]);
+    balance.account_id = __ATOUL(operation_arguments[1]);
+    strncpy(create.password, operation_arguments[2], MAX_PASSWORD_LEN);
+    create.password[MAX_PASSWORD_LEN] = '\0';
+    return create;
+}
+
+req_transfer_t new_transfer_message(char **operation_arguments) {
+    req_transfer_t transfer;
+    transfer.account_id = __ATOUL(operation_arguments[0]);
+    transfer.amount = __ATOUL(operation_arguments[1]);
+    return transfer;
+}
 
 
---
+++ user_src/protocol.h
@@ -0,0 +1,11 @@
+#define __USER_PROTOCOL_H__ //disables header
+#ifndef __USER_PROTOCOL_H__
+#define __USER_PROTOCOL_H__
+
+#include "types.h"
+
+char *serialize_request(tlv_request_t request);
+
+tlv_reply_t deserialize_reply(char *serialized_reply);
+
+#endif/*__USER_PROTOCOL_H__*/
