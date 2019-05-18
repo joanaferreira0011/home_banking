c7d461337a1c6c7ce8ba58920ec025d4e1461f03
João N. Matos <joao.nuno.matos@fe.up.pt>
2019-05-18 15:43:18 +0100

    More debugging

--- user_src/args.c
+++ user_src/args.c
@@ -26,6 +26,7 @@ req_transfer_t new_transfer_message(char **operation_arguments);
 
 tlv_request_t process_arguments(char **argv)
 {
+    __debug_log_str("args::process_arguments: entered function");
     req_value_t value;
     value.header = new_header(
         argv[ACCID_INDEX],
@@ -33,6 +34,7 @@ tlv_request_t process_arguments(char **argv)
         argv[OPDELAY_INDEX]);
     op_type_t operation = __ATOUL(argv[OPCODE_INDEX]);
     char **opargs = separate_args(argv[OPARGS_INDEX]);
+    __debug_log_str_array("Opargs array:", opargs);
     switch (operation) {
         case OP_CREATE_ACCOUNT:
             value.create = new_create_account_message(opargs);
@@ -83,22 +85,27 @@ char **separate_args(char *arg_str)
         }
     }
 
-    char *token = strtok(arg_str, " ");
+    char *token = strtok(arg_str, " "); /* First time invoked with actual string */
     strncpy(array[0], token, MAX_PASSWORD_LEN + 1);
     int index = 1;
-    while (((token = strtok(NULL, " ")) != NULL) && (index < 3))
+    __debug_print_str("Value of token: ");
+    __debug_log_str(token);
+    while (((token = strtok(NULL, " ")) != NULL) && (index < 3)) /* Subsequently invoked with NULL */
     {
+        __debug_print_str("Value of token: ");
+        __debug_log_str(token);
         strncpy(array[index], token, MAX_PASSWORD_LEN + 1);
         ++index;
     }
 
     array[3] = NULL;
-
+    __debug_log_str("args::separate_args: leaving function");
     return array;
 }
 
 void string_array_free_elements(char **array)
 {
+    __debug_log_str("args::string_array_free_elements: entered function");
     if (array == NULL)
     {
         return;
@@ -108,10 +115,12 @@ void string_array_free_elements(char **array)
         free(array[i]);
     }
     free(array);
+    __debug_log_str("args::strings_array_free_elements: leaving function");
 }
 
 req_header_t new_header(char *account_id, char *password, char *op_delay)
 {
+    __debug_log_str("args::new_header: entered function");
     req_header_t header;
     header.pid = getpid();
     header.account_id = __ATOUL(account_id);
@@ -119,24 +128,28 @@ req_header_t new_header(char *account_id, char *password, char *op_delay)
     strncpy(header.password, password, MAX_PASSWORD_LEN);
     header.password[MAX_PASSWORD_LEN] = '\0';
     header.op_delay_ms = __ATOUL(account_id);
-    ;
+    __debug_log_str("args::new_header: leaving function");
     return header;
 }
 
 req_create_account_t new_create_account_message(char **operation_arguments)
 {
+    __debug_log_str("args::new_create_account_message: entered function");
     req_create_account_t create;
     create.account_id = __ATOUL(operation_arguments[0]);
     create.balance = __ATOUL(operation_arguments[1]);
     strncpy(create.password, operation_arguments[2], MAX_PASSWORD_LEN);
     create.password[MAX_PASSWORD_LEN] = '\0';
+    __debug_log_str("args::new_create_account_message: leaving function");
     return create;
 }
 
 req_transfer_t new_transfer_message(char **operation_arguments)
 {
+    __debug_log_str("args::new_transfer_message: entered function");
     req_transfer_t transfer;
     transfer.account_id = __ATOUL(operation_arguments[0]);
     transfer.amount = __ATOUL(operation_arguments[1]);
+    __debug_log_str("args::new_transfer_message: leaving function");
     return transfer;
 }
--- user_src/debug.h
+++ user_src/debug.h
@@ -7,11 +7,13 @@
 #define __debug_print_str(str) fprintf(stderr, "%s", str)
 #define __debug_log_int(prefix, num) fprintf(stderr,"%s%d\n", prefix, num)
 #define __debug_log_uint(prefix, num) fprintf(stderr, "%s%u\n", prefix, num)
+void __debug_log_str_array(char* header, char **array);
 #else
 #define __debug_log_str(arg1)
 #define __debug_print_str(arg1)
 #define __debug_log_int(arg1, arg2)
 #define __debug_log_uint(arg1, arg2)
+#define __debug_log_str_array(arg1, arg2)
 #endif
 
 #endif/*__USER_DEBUG_H__*/
